c579b442ad403410b868d486e70189c7f0164158
Ángel Oreste <39156285+aorestr@users.noreply.github.com>
2018-07-15 19:58:57 +0200

    first interactions with GNS3
    I've added all the necessary assemblies. I've also try out the API by starting the whole GNS3 project using a singleton object

---
+++ Assets/Scripts/GNS3Handler.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GNS3sharp;
+
+public class GNS3Handler : MonoBehaviour {
+
+    public static GNS3Handler Instance { get; private set; } = null;
+    public GNS3sharp.GNS3sharp projectHandler = new GNS3sharp.GNS3sharp("575a3cf3-7ff3-4924-8fed-9df4e6fef351");
+
+    //Awake is always called before any Start functions
+    void Awake() {
+        if (Instance == null) Instance = this;
+        else if (Instance != this)  Destroy(gameObject);
+    }
+}
---
+++ Assets/Scripts/L1M1Handler.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class L1M1Handler : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        GNS3Handler.Instance.projectHandler.StartProject();
+	}
+}
